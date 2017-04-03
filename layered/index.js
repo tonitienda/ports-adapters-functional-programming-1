87195a74e1628c2e8717b472c80227190453e803
Toni Tienda <tonitienda@gmail.com>
2017-04-04 08:17:30 +0900

    Adding empty http facade to Layered Architecture

---
+++ layered/index.js
@@ -0,0 +1,15 @@
+
+const express = require('express');
+const app = express();
+
+app.post('registerUser', (req, res) => {
+    
+});
+
+app.get('confirmUser', (req, res) => {
+
+});
+
+app.listen(3000, () => {
+    console.log('Listening to port 3000');
+});
