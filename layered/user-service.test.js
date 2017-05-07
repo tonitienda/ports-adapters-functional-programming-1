e6098f248ddba257acec40620e14458242f21825
Toni Tienda <tonitienda@gmail.com>
2017-05-07 20:11:54 +0900

    Adding unit tests to the user service.

--- layered/user-service.test.js
+++ layered/user-service.test.js
@@ -5,12 +5,33 @@ const chaiAsPromised = require('chai-as-promised')
 chai.use(chaiAsPromised);
 const expect = chai.expect;
 
+const doNothing = () => {};
+const returnArgument = (arg) => arg;
 
 describe('user-service', () => {
     describe('registerUser', () => {
         it('If user data is null, registerUser should throw an exception', () => {
             const userService = new UserService();
             expect(userService.registerUser(null)).to.be.rejectedWith(Error);
+        });
+
+        it('If user data is null, registerUser should throw an exception', () => {
+            const userService = new UserService();
+            expect(userService.registerUser(null)).to.be.rejectedWith(Error);
+        });
+
+        it('If user data is correct and the user can be saved and the event published the service will return the new user', async () => {
+            const userData = {
+                userName: 'user-name',
+                password: 'some-password'
+            }
+            
+            const userService = new UserService({ save: returnArgument }, { publish: doNothing });
+            const newUser = await userService.registerUser(userData);
+
+            expect(newUser.userName).to.equal(userData.userName);
+            expect(newUser.password).to.equal(userData.password);
+            expect(newUser.state).to.equal('PENDING');
         });
     })
 })
